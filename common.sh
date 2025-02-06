d0b8947456ce14dc747a1925aea4500b003cd1e1
stasbucik <53035783+stasbucik@users.noreply.github.com>
2025-02-06 21:21:36 +0100

    Update environment for version 2024.2
    Signed-off-by: stasbucik <53035783+stasbucik@users.noreply.github.com>

--- common.sh
+++ common.sh
@@ -1,9 +1,9 @@
-ubuntu_version="jammy-20230301"
+ubuntu_version="noble-20240423"
 tini_version="v0.19.0"
 tini_sig_key="595E85A6B1B4779EA4DAAEC70B588DFF0527A9B7"
-petalinux_version="2023.2"
+petalinux_version="2024.2"
 image_name="ubuntu-$ubuntu_version-petalinux-$petalinux_version"
 container_name="$image_name-buildenv"
 
-INST_DIR=/opt/Xilinx/petalinux/2023.2
-PETALINUX_INSTALLER_NAME=petalinux-v2023.2-10121855-installer.run
+INST_DIR=/opt/Xilinx/petalinux/2024.2
+PETALINUX_INSTALLER_NAME=petalinux-v2024.2-11062026-installer.run
