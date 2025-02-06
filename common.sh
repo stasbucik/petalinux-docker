1d5a150fd2bbdb0aa66ee9a7c099a2e06c58ec34
stasbucik <53035783+stasbucik@users.noreply.github.com>
2025-02-06 21:17:31 +0100

    Update environment for version 2021.1
    Signed-off-by: stasbucik <53035783+stasbucik@users.noreply.github.com>

--- common.sh
+++ common.sh
@@ -1,9 +1,9 @@
-ubuntu_version="bionic-20200219"
+ubuntu_version="focal-20200916"
 tini_version="v0.19.0"
 tini_sig_key="595E85A6B1B4779EA4DAAEC70B588DFF0527A9B7"
-petalinux_version="2020.1"
+petalinux_version="2021.1"
 image_name="ubuntu-$ubuntu_version-petalinux-$petalinux_version"
 container_name="$image_name-buildenv"
 
-INST_DIR=/opt/Xilinx/petalinux/2020.1
-PETALINUX_INSTALLER_NAME=petalinux-v2020.1-final-installer.run
+INST_DIR=/opt/Xilinx/petalinux/2021.1
+PETALINUX_INSTALLER_NAME=petalinux-v2021.1-final-installer.run
